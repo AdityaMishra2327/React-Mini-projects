de6334a777a9313dc17f46d5aab74cba7ac0e8cc
AdityaMishra2327 <idadityamishraji@gmail.com>
2024-07-13 10:35:30 +0530

    Final

---
+++ npmdigitalclock/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Clock from 'react-digital-clock';
+
+const App = () =>{
+  return(
+    <div className="clock">
+      <Clock/>
+    </div>
+);
+  
+}
+export default App;
